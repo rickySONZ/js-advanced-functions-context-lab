e84b5e667a24f3f6ed0f1e27c43cc76d28628b35
Ryan Erricson <ryanerricson@gmail.com>
2021-05-14 10:55:27 -0400

    commit tree ed04efaae817714602441477eab5a1b547879bf1

--- index.js
+++ index.js
@@ -9,6 +9,64 @@
  for you to use if you need it!
  */
 
+ // Creating employee record
+
+ const createEmployeeRecord = (array) => {
+    let r;
+    return r = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [], 
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecords){
+    return employeeRecords.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(time){
+    let date = time.split(" ")[0]
+    let hour = time.split(" ")[1]
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+function createTimeOutEvent(time){
+    let date = time.split(" ")[0]
+    let hour = time.split(" ")[1]
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let x = this.timeInEvents.find(function(e){ 
+        return e.date === date
+    })
+    let y = this.timeOutEvents.find(function(e){ 
+        return e.date === date
+    })
+    return (y.hour - x.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    let earnedWage = hoursWorkedOnDate.call(this, date)
+    let pay = this.payPerHour
+    return parseInt(earnedWage * pay)
+ }
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +77,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(array, name){
+    return array.find(e => e.firstName == name)
+}
+
+function calculatePayroll(array){
+    return array.reduce(function(total, e){
+        return total + allWagesFor.call(e)
+    }, 0)
 }
